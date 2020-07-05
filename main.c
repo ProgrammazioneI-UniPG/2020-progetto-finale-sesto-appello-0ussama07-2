2e0082d977b8ae8f61533787db8123d423a4a43c
0ussama07 <57435305+0ussama07@users.noreply.github.com>
2020-07-06 00:06:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,48 @@
-#include "gamelib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "utility.h"
+#include "game.h"
 
-// Questo file continene solo la definizione della funzione main
-// con il menu principale 1-2-3
+int main()
+{
+	init_rand();
 
-int main( ) {
+	int invalid = 0;
 
+	do
+	{
+		invalid = 0;
 
+		clear_output();
+		printf(" ============ Arvais vs Hartornen ============\n");
+		printf(" [1] Create Tunnels\n");
+		printf(" [2] Play\n");
+		printf(" [3] End Game\n");
+		printf(" =============================================\n");
 
+		if (invalid == 1)
+		{
+			printf(" Invalid command. Try again!\n");
+		}
+
+		int command = 0;
+
+		printf(" >>> ");
+		_ = scanf("%d", &command);
+
+		switch (command)
+		{
+		case 1:
+			create_tunnels();
+			break;
+		case 2:
+			play();
+			break;
+		case 3:
+			end_game();
+			return 0;
+		default:
+			invalid = 1;
+		}
+	} while (1);
 }
-
-
-
