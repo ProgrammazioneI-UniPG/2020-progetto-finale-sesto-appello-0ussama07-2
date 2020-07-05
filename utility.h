bb81330e15ae594da8b5d5ee094ad443cc453a53
0ussama07 <57435305+0ussama07@users.noreply.github.com>
2020-07-06 00:09:00 +0200

    Create utility.h

---
+++ utility.h
@@ -0,0 +1,16 @@
+#ifndef UTILITY_HEADER
+#define UTILITY_HEADER
+
+int _;
+
+void init_rand();
+
+int within(int value, int min, int max);
+
+void clear_input();
+
+void clear_output();
+
+void press_any_key(const char* action);
+
+#endif // !UTILITY_HEADER
